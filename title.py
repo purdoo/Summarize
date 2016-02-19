c6cde7c52164b5d473821ec009849e6ee5de4638
Chris <chrisliow@gmail.com>
2016-02-18 20:34:42 -0600

    More additions and shit

--- title.py
+++ title.py
@@ -1,6 +1,12 @@
 """
   Methods associated with incorporating the article title or subsection titles into the Summarization algorithm
+
+  Certain values are currently hardcoded in (such as lists for punctuation and 'empty' words) The eventual solution is to store externally or through a DB.
 """
+
+# Hardcoded 'Lists'
+punct = [',','.',':',';','\'','\"','|']
+ignore = ['and', 'the', 'but', 'so', 'like', 'be']
 
 def getArticleTitle(path):
   with open(path, 'r') as fin:
@@ -8,9 +14,25 @@ def getArticleTitle(path):
   return title.strip()
 
 def getTitleWords(title):
+  words = []
   for word in title.split(' '):
-    print(word)
+    #print(word)
+    words.append(word)
+  return words
+
+def findAbbr(title):
+  pass
+
+def reduceTitle(words):
+  reduced = words  
+  for word in reduced:
+    for p in punct: # there has to be a more efficient way of doing this...
+      word.strip(p)
+    if word in ignore:
+      reduced.remove(word)
+  return reduced   
+
 if __name__ == "__main__":
   title = getArticleTitle('Articles/Zika.txt')
-  print(title)
-  getTitleWords(title)
+  words = getTitleWords(title)
+  print(reduceTitle(words))
